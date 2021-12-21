89ee54e25f331cb640a3dc930afc83d5330554e0
ParadoxFlame <shivammaji5818@gmail.com>
2021-12-20 16:39:16 -0800

    Migration from Snack to GitHub

---
+++ App.js
@@ -0,0 +1,58 @@
+//IMPORTANT
+// IF YOU WISH TO CHECK OUT THE APP
+// SIGN IN WITH THESE CREDENTIALS
+// stest@gmail.com
+// 123456
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import BookTransactionScreen from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+import LoginScreen from './screens/LoginScreen';
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Transaction: { screen: BookTransactionScreen },
+    Search: { screen: SearchScreen },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const routeName = navigation.state.routeName;
+        console.log(routeName);
+        if (routeName === 'Transaction') {
+          return (
+            <Image
+              source={require('./assets/book.png')}
+              style={{ width: 40, height: 40 }}
+            />
+          );
+        } else if (routeName === 'Search') {
+          return (
+            <Image
+              source={require('./assets/searchingbook.png')}
+              style={{ width: 40, height: 40 }}
+            />
+          );
+        }
+      },
+    }),
+  }
+);
+
+var AppNavigator = createSwitchNavigator({
+  LoginScreen: { screen: LoginScreen },
+  TabNavigator: { screen: TabNavigator },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
